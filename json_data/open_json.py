52343f2ded6e6063d9ef6fc35b6f17e165576885
Artyom Zaharov <73885381+Arteww@users.noreply.github.com>
2021-12-04 09:17:40 +0700

    Update open_json.py

--- json_data/open_json.py
+++ json_data/open_json.py
@@ -2,10 +2,6 @@ import os
 from json import load
 
 path_to_pictures_json = f'{os.path.dirname(os.path.abspath(__file__))}/pict.json'
-path_to_users_json = f'{os.path.dirname(os.path.abspath(__file__))}/users.json'
 
 with open(path_to_pictures_json, 'r') as file:
     pictures = load(file)
-
-with open(path_to_users_json, 'r') as file:
-    users = load(file)
